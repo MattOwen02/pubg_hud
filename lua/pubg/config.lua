ea9a108b06b644b102c825769fcab354bf335d71
Matt <48577498+MattOwen02@users.noreply.github.com>
2019-03-14 23:21:30 -0400

    Create config.lua

---
+++ lua/pubg/config.lua
@@ -0,0 +1,34 @@
+//////////////////////////////////
+//        Config Settings       //
+//////////////////////////////////
+DarkRPVariables = true
+
+HUDOverhead = true
+
+ChatCommand = "!pubg"
+
+WeaponsNotDraw = {/* Add Custom Weapons Here -->*/ "", "", 
+/* Default No Draw --> */"weapon_physgun", "keys", "gmod_tool", "weapon_medkit", "weapon_fists", "weapon_physcannon", "weapon_stunstick", "weapon_bugbait", "weapon_crowbar","med_kit", "lockpick", "door_ram", "weaponchecker", "unarrest_stick", "arrest_stick", "weapon_keypadchecker", "pocket", "stunstick", "manhack_welder"}
+
+// Custom Suicide Messages (If players kills themself)
+randomSuicideMsg = true // The default messages are gruesome, and offensive to some people
+
+suicideMsg = {
+	"overdosed on super powerful synthetic marijuana",
+	"jumped into 50 tons of molten iron",
+	"stepped into a tiger's den",
+	"flew their airplane into Mount Rushmore",
+	"took a shower at Auschwitz",
+	"walked into the olympic flame"
+}
+
+// Custom Death Messages (If player is killed by other player)
+randomDeathMsg = true // The default messages are gruesome, and offensive to some people
+
+deathMsg = { // Format of message: {VICTIM} deathMsg {ATTACKER}
+	"was viciously cannibalized by",
+	"got head blown off by",
+	"ate a .50 caliber bullet from",
+	"was penetrated to hard by",
+	"got his brain splattered on the wall by"
+}
